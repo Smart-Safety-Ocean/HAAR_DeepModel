d8650a0828f9910c537cf89a0c6d5a30e9a547dc
HongDoubleS <sungsamhong@rabahgroow.com>
2022-11-21 14:46:49 +0900

    v 0.85 deleted adet (license problem)

--- AdelaiDet/adet/__init__.py
+++
@@ -1,3 +0,0 @@
-from adet import modeling
-
-__version__ = "0.1.1"
--- AdelaiDet/adet/layers/csrc/cuda_version.cu
+++
@@ -1,7 +0,0 @@
-#include <cuda_runtime_api.h>
-
-namespace adet {
-int get_cudart_version() {
-  return CUDART_VERSION;
-}
-} // namespace adet
